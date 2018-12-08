36b729c3c978c6062732c09a0a5891b4cb6566ac
Dewi Ayu Nirmalasari <dnirmalasari18@gmail.com>
2018-12-08 21:46:33 +0700

    posisi z partikel hanya 1

--- libs/something.js
+++ libs/something.js
@@ -1,23 +1,11 @@
 var scene, camera,fieldOfView,aspesctRatio,nearPlane,farPlane,shadowLight,light,renderer,container,
     HEIGHT,WIDTH,windowHalfX,windowHalfY,xLimit,yLimit;
 
-// PARTICLES COLORS
-var colors = ['#dff69e', 
-              '#00ceff', 
-              '#002bca', 
-              '#ff00e0', 
-              '#3f159f', 
-              '#71b583', 
-              '#00a2ff'];
-
 // PARTICLES
-// as the particles are recycled, I use 2 arrays to store them
-// flyingParticles used to update the flying particles and waitingParticles used to store the "unused" particles until we need them;
+var colors = ['#dff69e', '#00ceff', '#002bca', '#ff00e0', '#3f159f', '#71b583', '#00a2ff'];
 var flyingParticles = []; 
 		waitingParticles = [];
-// maximum z position for a particle
-		maxParticlesZ = 600; 
-
+    zLocationParticles=0;
 // SPEED
 var speed = {x:0, y:0};
 var smoothing = 10;
@@ -26,7 +14,6 @@ var smoothing = 10;
 var mousePos = {x:0, y:0};
 var stats;
 var halfPI = Math.PI/2;
-
 
 function init(){
   scene = new THREE.Scene();
@@ -44,27 +31,17 @@ function init(){
   renderer.setSize(WIDTH, HEIGHT);
   container = document.getElementById('world');
   container.appendChild(renderer.domElement);
-   
-  /*
-  As I will recycle the particles, I need to know the left and right limits they can fly without disappearing from the camera field of view.
-  As soon as a particle is out of the camera view, I can recycle it : remove it from the flyingParticles array and push it back in the waitingParticles array.
-  I guess I can do that by raycasting each particle each frame, but I think this will be too heavy. Instead I prefer to precalculate the x coordinate from which a particle is not visible anymore. But this depends on the z position of the particle.
-  Here I decided to use the furthest possible z position for a particle, to be sure that all the particles won't be recycled before they are out of the camera view. But this could be much more precise, by precalculating the x limit for each particle depending on its z position and store it in the particle when it is "fired". But today, I'll keep it simple :) 
-  !!!!!! I'm really not sure this is the best way to do it. If you find a better solution, please tell me  
-  */
   
   // convert the field of view to radians
   var ang = (fieldOfView/2)* Math.PI / 180; 
   // calculate the max y position seen by the camera related to the maxParticlesZ position, I start by calculating the y limit because fielOfView is a vertical field of view. I then calculate the x Limit
-  yLimit = (camera.position.z + maxParticlesZ) * Math.tan(ang); // this is a formula I found, don't ask me why it works, it just does :) 
+  yLimit = (camera.position.z + zLocationParticles) * Math.tan(ang); // this is a formula I found, don't ask me why it works, it just does :) 
   // Calculate the max x position seen by the camera related to the y Limit position
   xLimit = yLimit *camera.aspect;
    
-  // precalculate the center of the screen, used to update the speed depending on the mouse position
   windowHalfX = WIDTH / 2;
   windowHalfY = HEIGHT / 2;
 
- // handling resize and mouse move events
   window.addEventListener('resize', onWindowResize, false);
   document.addEventListener('mousemove', handleMouseMove, false);
 }
@@ -79,7 +56,7 @@ function onWindowResize() {
   camera.updateProjectionMatrix(); // force the camera to update its aspect ratio
   // recalculate the limits
  	var ang = (fieldOfView/2)* Math.PI / 180; 
-  yLimit = (camera.position.z + maxPartcilesZ) * Math.tan(ang); 
+  yLimit = (camera.position.z + zLocationParticles) * Math.tan(ang); 
   xLimit = yLimit *camera.aspect;
 }
 
@@ -115,7 +92,6 @@ function updateSpeed(){
 }
 
 function loop() {  
-  // particles update 
   for (var i=0; i<flyingParticles.length; i++){
     var particle = flyingParticles[i];
     particle.rotation.y += (1/particle.scale.x) *.05;
@@ -123,12 +99,14 @@ function loop() {
     particle.rotation.z += (1/particle.scale.x) *.05;
     particle.position.x += -10 -(1/particle.scale.x) * speed.x *.2;
     particle.position.y += (1/particle.scale.x) * speed.y *.2;
-    if (particle.position.x < -xLimit - 80){ // check if the particle is out of the field of view
+    if (particle.position.x < -xLimit - 80){ 
       scene.remove(particle);
-      waitingParticles.push(flyingParticles.splice(i,1)[0]); // recycle the particle
+      waitingParticles.push(flyingParticles.splice(i,1)[0]); 
       i--;
     }
   }
+
+  
   renderer.render(scene, camera);
   stats.update();
   requestAnimationFrame(loop);
@@ -143,9 +121,9 @@ function createStats() {
 }
 
 
-// Lights
-// I use 2 lights, an hemisphere to give a global ambient light
-// And a harder light to add some shadows
+//Lights
+//an hemisphere to give a global ambient light
+//harder light to add some shadows
 function createLight() {
   light = new THREE.HemisphereLight(0xffffff, 0xffffff, .3)
   scene.add(light);
@@ -154,26 +132,24 @@ function createLight() {
  	scene.add(shadowLight);
 }
 
-// PARTICLES
 function createParticle(){
   var particle, geometryCore, ray, w,h,d, sh, sv;
   
-  // 3 different shapes are used, chosen randomly
   var rnd = Math.random();
   
-  // BOX
+  //box
   if (rnd<.33){
     w = 10 + Math.random()*30;
     h = 10 + Math.random()*30;
     d = 10 + Math.random()*30;
     geometryCore = new THREE.BoxGeometry(w,h,d);
   }
-  // TETRAHEDRON
+  // Tetrahedron
   else if (rnd<.66){
     ray = 10 + Math.random()*20;
     geometryCore = new THREE.TetrahedronGeometry(ray);
   }
-  // SPHERE... but as I also randomly choose the number of horizontal and vertical segments, it sometimes lead to wierd shapes
+  // sphere (but random on segment vert and hor)
   else{
     ray = 5+Math.random()*30;
     sh = 2 + Math.floor(Math.random()*2);
@@ -181,7 +157,6 @@ function createParticle(){
     geometryCore = new THREE.SphereGeometry(ray, sh, sv);
   }
   
-  // Choose a color for each particle and create the mesh
   var materialCore = new THREE.MeshLambertMaterial({
     color: getRandomColor(),
     shading: THREE.FlatShading
@@ -190,7 +165,8 @@ function createParticle(){
   return particle;
 }
 
-// depending if there is particles stored in the waintingParticles array, get one from there or create a new one
+
+
 function getParticle(){
   if (waitingParticles.length) {
     return waitingParticles.pop();
@@ -201,10 +177,9 @@ function getParticle(){
 
 function flyParticle(){
   var particle = getParticle();
-  // set the particle position randomly but keep it out of the field of view, and give it a random scale
   particle.position.x = xLimit;
   particle.position.y = -yLimit + Math.random()*yLimit*2;
-  particle.position.z = Math.random()*maxParticlesZ;
+  particle.position.z = zLocationParticles;
   var s = .1 + Math.random();
   particle.scale.set(s,s,s);
   flyingParticles.push(particle);
@@ -228,10 +203,9 @@ function hexToRgb(hex) {
   } : null;
 }
 
-
 init();
 createStats();
 createLight();
 createParticle();
 loop();
-setInterval(flyParticle, 70); // launch a new particle every 70ms
+setInterval(flyParticle, 100);
